4c516d04fe6c683106f678a4563dba48097a97e6
Uttam Kumar Pal <uttampal.kumar1996@gmail.com>
2023-02-12 22:54:13 +0600

    Add Permission Package Spatie and create UserSeeder and RoleSeeder

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name' => 'Admin',
+            'email' => 'admin@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('admin009'),
+        ])->assignRole('admin');
+        User::create([
+            'name' => 'Srudent',
+            'email' => 'srudent@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('srudent009'),
+        ])->assignRole('student');
+        User::create([
+            'name' => 'Staff',
+            'email' => 'staff@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('Staff009'),
+        ])->assignRole('staff');
+        User::create([
+            'name' => 'Professor',
+            'email' => 'professor@gmail.com',
+            'email_verified_at' => now(),
+            'password' => bcrypt('professor009'),
+        ])->assignRole('professor');
+    }
+}
